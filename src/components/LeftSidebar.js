4c4639b1b198df93c2667edbb34998110caadb7b
Marcelo Tena <marcelotena@fastandyours.com>
2020-05-21 10:37:03 +0200

    First commit, base app layout

---
+++ src/components/LeftSidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSidebar = () => {
+  return (
+      <div className="App-left-sidebar">
+        Left Sidebar
+      </div>
+  );
+};
+
+export default LeftSidebar;
---
+++ src/components/RightSidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSidebar = () => {
+  return (
+      <div className="App-right-sidebar">
+        Right Sidebar
+      </div>
+  );
+};
+
+export default RightSidebar;
